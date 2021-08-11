9da3d3453dfa85c96d5fbf9f2f192755d161155b
Ghost Tall <diarnotall@gmail.com>
2021-08-11 14:58:20 +0000

    Test Api with Advenced Rest Client

--- Api/App.py
+++ Api/App.py
@@ -1,7 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+class Tests(Resource):
+    def get(self):
+        return {'methods': 'GET'}
+
+    def post(self):
+        some_json = request.get_json()
+        return {'methods': 'POST'}, 201
+
+api.add_resource(Tests, "/")
+
+"""
+@app.route("/", methods=["GET", "POST"])
+def hello():
+    if(request.method == "POST"):
+        some_json = request.get_json()
+        return jsonify({"M": "POST"}), 201
+    else:
+        return jsonify({"M": "GET"})
+"""
