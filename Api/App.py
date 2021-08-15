f2f594ea4b465972e8b95ccf37e63ddaf32cb1d3
Ghost Tall <diarnotall@gmail.com>
2021-08-15 16:29:36 +0000

    send data from flask to angular

--- Api/App.py
+++ Api/App.py
@@ -1,25 +1,53 @@
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-class Tests(Resource):
-    def get(self):
-        return {'methods': 'GET'}
+data = [
+  [
+    "Python", 
+    315
+  ], 
+  [
+    "intermediate", 
+    167
+  ], 
+  [
+    "python", 
+    161
+  ], 
+  [
+    "basics", 
+    118
+  ], 
+  [
+    "web-dev", 
+    108
+  ], 
+  [
+    "data-science", 
+    51
+  ], 
+  [
+    "best-practices", 
+    49
+  ], 
+  [
+    "advanced", 
+    45
+  ], 
+  [
+    "django", 
+    43
+  ], 
+  [
+    "flask", 
+    41
+  ]
+]
 
-    def post(self):
-        some_json = request.get_json()
-        return {'methods': 'POST'}, 201
-
-api.add_resource(Tests, "/")
-
-"""
-@app.route("/", methods=["GET", "POST"])
-def hello():
-    if(request.method == "POST"):
-        some_json = request.get_json()
-        return jsonify({"M": "POST"}), 201
-    else:
-        return jsonify({"M": "GET"})
-"""
+@app.route('/users', methods=['GET'])
+def getUsers():
+    return jsonify(data)
