2dd9cdf0de3df0c42531eb1d7ce71162ade563c9
Ghost Tall <diarnotall@gmail.com>
2021-08-19 13:43:31 +0000

    Structure web configuration

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { AppService } from './app.service';
-import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -8,28 +6,6 @@ import { User } from './user';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web';
-  data: User[] = [];
-  users: User[] = [];
-  constructor(private appservice: AppService){}
-
-  ngOnInit() {
-    this.getData();
-    this.getUsers();
-  }
-
-  getData() {
-    this.appservice.getData().subscribe(data => {
-      this.data = data;
-      console.log(this.data);
-    });
-  }
-
-  getUsers() {
-    this.appservice.getUsers().subscribe(data => {
-      this.users = data;
-      console.log(this.data);
-    });
-  }
+  title = 'DAWINI';
 
 }
