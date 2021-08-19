a51f7b6c63a54c3c3331016ab88ccbad8393b86f
Ghost Tall <diarnotall@gmail.com>
2021-08-19 13:37:57 +0000

    Create all important component

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { InscriptionComponent } from './pages/inscription/inscription.component';
+import { BaseComponent } from './common/base/base.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    InscriptionComponent,
+    BaseComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
