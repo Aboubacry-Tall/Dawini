05cd37f5e9ae3d073f3b1a900cc5c5da6f5c31c3
Diarno Tall <diarnotall@gmail.com>
2021-09-12 13:11:41 -0700

    create map leaflet

---
+++ web/src/app/modules/locate/maplef/maplef.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-maplef',
+  templateUrl: './maplef.component.html',
+  styleUrls: ['./maplef.component.css']
+})
+export class MaplefComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
