aab50e8b9e4cd6c1a086b698c32ed2006a69279f
Diarno Tall <diarnotall@gmail.com>
2021-09-20 00:26:28 -0700

    User Interface

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MedicamentComponent } from './modules/medicaments/medicament/medicament.component';
+import { MedicamentsComponent } from './modules/medicaments/medicaments/medicaments.component';
+import { PharmacieComponent } from './modules/pharmacies/pharmacie/pharmacie.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { ResetComponent } from './pages/reset/reset.component';
+import { SearchComponent } from './pages/search/search.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'reset', component: ResetComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'search', component: SearchComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'pharmacie', component: PharmacieComponent},
+  {path: 'medicament', component: MedicamentComponent},
+  {path: 'medicaments', component: MedicamentsComponent}
   
 ];
 
