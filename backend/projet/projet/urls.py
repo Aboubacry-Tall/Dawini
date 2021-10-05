1f0cc3c9ed37c94fef8dd71bc76b0aaaaf627261
cheikh22 <cheikh.raby24@gmail.com>
2021-10-05 15:16:13 +0000

    creation d'un app django

--- backend/projet/projet/urls.py
+++ backend/projet/projet/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from fullapi import views
+from mobileapp import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'medicament', views.MedicamentViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
