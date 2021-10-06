aaf8e61aa1742c19f3dbce69ae30481f261e53c2
Diarno Tall <diarnotall@gmail.com>
2021-10-06 16:04:26 +0100

    reset chico test

--- backend/projet/mobileapp/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Medicament(models.Model):
-    nom = models.CharField(max_length=50,null=False, blank=False)
-    description = models.TextField()
-    prix = models.IntegerField()
-
-    def __str__(self):
-        return self.nom
--- backend/projet/mobileapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import User 
-from rest_framework import serializers
-from .models import Medicament
-
-class MedicamentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Medicament
-        fields = ['id', 'nom', 'description', 'prix']
