ac2ed34555916e1f955958321ec224fa1f392047
Diarno Tall <diarnotall@gmail.com>
2021-10-11 21:03:01 +0100

    add login-pharmacie url

--- backend/backoffice/web/urls.py
+++ backend/backoffice/web/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from web import views 
  
 urlpatterns = [ 
-    url(r'^api/create-pharmacie', views.create_pharmacie)
+    url(r'^api/create-pharmacie', views.create_pharmacie),
+    url(r'^api/login-pharmacie', views.login_pharmacie)
 ]
