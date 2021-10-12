5e840484c92f0f8a13a63782f5070e18f03e621c
Diarno Tall <diarnotall@gmail.com>
2021-10-12 11:57:35 +0100

    alter table pharmacie

--- backend/backoffice/web/models.py
+++ backend/backoffice/web/models.py
@@ -3,6 +3,13 @@ from django.db import models
 class Pharmacie(models.Model):
     nom = models.CharField(max_length=50, blank=False, default='')
     pseudo = models.CharField(max_length=50, blank=True, default='')
-    email = models.CharField(max_length=50, blank=True, default='')
+    email = models.EmailField(max_length=50, blank=True, default='')
     password = models.CharField(max_length=50, blank=True, default='')
     quartier = models.CharField(max_length=50, blank=True, default='')
+
+class Medicament(models.Model):
+    nom = models.CharField(max_length=50, blank=False, default='')
+    prix = models.CharField(max_length=50, blank=False, default='')
+    code = models.CharField(max_length=50, blank=False, default='')
+    etat = models.CharField(max_length=1, blank=False, default='')
+    description = models.CharField(max_length=255, blank=False, default='')
