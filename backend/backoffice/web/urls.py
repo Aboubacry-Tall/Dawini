fb158e46a95541b15211aa6e77171a85c2b75919
Diarno Tall <diarnotall@gmail.com>
2021-10-13 11:44:17 +0100

    urlpatterns for medicaments

--- backend/backoffice/web/urls.py
+++ backend/backoffice/web/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from web import views 
  
 urlpatterns = [ 
-    url(r'^api/create-pharmacie', views.create_pharmacie),
-    url(r'^api/login-pharmacie', views.login_pharmacie)
+    url(r'^api/web/create-pharmacie', views.create_pharmacie),
+    url(r'^api/web/login-pharmacie', views.login_pharmacie),
+    url(r'^api/web/create-medicament', views.create_medicament),
+    url(r'^api/web/medicaments', views.get_all_medicaments)
 ]
