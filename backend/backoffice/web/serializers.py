7ae89ae2f6bcd42ccc82ccbcc1fe80cffdedc21f
Diarno Tall <diarnotall@gmail.com>
2021-10-13 11:43:56 +0100

    medicament serializer

--- backend/backoffice/web/serializers.py
+++ backend/backoffice/web/serializers.py
@@ -1,9 +1,16 @@
 
-from rest_framework import serializers 
+from rest_framework import serializers
 from web.models import Pharmacie
+from web.models import Medicament
 
 class PharmacieSerializer(serializers.ModelSerializer):
  
     class Meta:
         model  = Pharmacie
+        fields = '__all__'
+
+class MedicamentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model  = Medicament
         fields = '__all__'
