ab7f0ac5f079537b29020568bda277f11a9c07ec
Diarno Tall <diarnotall@gmail.com>
2021-10-13 11:43:34 +0100

    model medicament

--- backend/backoffice/web/models.py
+++ backend/backoffice/web/models.py
@@ -9,7 +9,7 @@ class Pharmacie(models.Model):
 
 class Medicament(models.Model):
     nom = models.CharField(max_length=50, blank=False, default='')
-    prix = models.CharField(max_length=50, blank=False, default='')
+    prix = models.IntegerField(max_length=50, blank=False, default='')
     code = models.CharField(max_length=50, blank=False, default='')
-    etat = models.CharField(max_length=1, blank=False, default='')
+    etat = models.IntegerField(max_length=1, blank=False, default=0)
     description = models.CharField(max_length=255, blank=False, default='')
