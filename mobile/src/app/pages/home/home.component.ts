97f023a41ee71436cd5e912e71fba6edc1481af7
cheikh22 <cheikh.raby24@gmail.com>
2021-12-01 12:42:18 +0000

    delete asking permission in home page and update database sqlite3

--- mobile/src/app/pages/home/home.component.ts
+++ mobile/src/app/pages/home/home.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AndroidPermissions } from '@ionic-native/android-permissions/ngx';
-import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 
 @Component({
   selector: 'app-home',
@@ -9,48 +7,9 @@ import { LocationAccuracy } from '@ionic-native/location-accuracy/ngx';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(
-    private locationAccuracy: LocationAccuracy,  
-    private androidPermissions: AndroidPermissions) { }
+  constructor() { }
 
   ngOnInit() {
-    this.checkPermission()
-  }
-  
-  checkPermission() {
-    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION).then(
-      result => {
-        if (result.hasPermission) {
-          this.enableGPS();
-        } else {
-          this.locationAccPermission();
-        }
-      },
-      error => {console.log()}
-    );
-  }
-
-  locationAccPermission() {
-    this.locationAccuracy.canRequest().then((canRequest: boolean) => {
-      if (canRequest) {
-      } else {
-        this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.ACCESS_COARSE_LOCATION)
-          .then(
-            () => {
-              this.enableGPS();
-            },
-            error => {console.log()}
-          );
-      }
-    });
-  }
-
-  enableGPS() {
-    this.locationAccuracy.request(this.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY).then(
-      () => {
-      },
-      error => console.log()
-    );
   }
 
 }
