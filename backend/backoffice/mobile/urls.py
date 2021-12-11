9ce831d522d001e7a3198b33913152e1cd69d307
Diarno Tall <diarnotall@gmail.com>
2021-12-11 17:55:25 +0000

    Delete web app and mobile app

--- backend/backoffice/mobile/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url 
-from mobile import views 
- 
-urlpatterns = [ 
-    url(r'^api/mobile/medicaments$', views.medicament_list),
-    url(r'^api/mobile/medicaments/<str:nom>', views.medicament_search),
-    url(r'^api/mobile/pharmacies', views.pharmacie_list),
-]
